8604a59e58505bf1f3c81c238bba81ce078ee804
Katia-Leao <katiaska@gmail.com>
2022-12-16 12:08:07 -0300

    Modules

---
+++ Formation Course/lesson-4/4-Modules/src/server.ts
@@ -0,0 +1,65 @@
+import express from "express";
+
+const server = express();
+
+server.use(express.json());
+
+let products: any = [
+  {
+    id: 1,
+    name: "O Um anel",
+    description:
+      "chocolate amargo 70% folhado a ouro servido num porta jóias de brigadeiro de frutas vermelhas",
+    price: 50,
+    quantity: 10,
+  },
+];
+
+server.get("/", (request, response) => {
+  return response.send(products);
+});
+
+server.get("/:id", (request, response) => {
+  // a linha de baixo é igual a const id = request.params.id. Isso se chama desestruturação.
+  const { id } = request.params;
+  const product = products.find((x) => x.id === Number(id));
+
+  if (!product) {
+    return response.send("Product not found!");
+  }
+
+  return response.send(product);
+});
+
+server.post("/", (request, response) => {
+  const product = request.body;
+  products.push(product);
+  return response.send(products);
+});
+
+server.put("/:id", (request, response) => {
+  const { id } = request.params;
+  const productIndex = products.findIndex((x) => x.id === Number(id));
+
+  if (productIndex === -1) {
+    return response.send("Not found!");
+  }
+
+  products[productIndex].name = request.body.name;
+  products[productIndex].description = request.body.description;
+  products[productIndex].price = request.body.price;
+  products[productIndex].quantity = request.body.quantity;
+
+  return response.send(products[productIndex]);
+});
+
+server.delete("/:id", (request, response) => {
+  const { id } = request.params;
+  products = products.filter((x) => x.id !== Number(id));
+  return response.send("Deleted");
+});
+
+const port = 3333;
+server.listen(port, () => {
+  console.log(`servidor rodando na porta ${port}`);
+});
